b6db44680592c3d432ebb738734ee0c551720454
Jooho Lee <jlee@redhat.com>
2018-09-22 16:36:32 -0400

    thread 10000

--- src/main/java/hello/DemoController.java
+++ src/main/java/hello/DemoController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 @RestController
 public class DemoController {
-
+    int i=0;
     @RequestMapping("/hello")
     public String index(){
         return "Hello World!";
@@ -20,7 +21,7 @@ public class DemoController {
         Runnable myRunnable = new Runnable(){
 
             public void run(){
-                int i=0;
+
                 System.out.println("Creating object");
                 while(true){
                     ArrayList list = new ArrayList(100);
@@ -30,8 +31,17 @@ public class DemoController {
             }
         };
 
-        Thread thread = new Thread(myRunnable);
-        thread.start();
+        IntStream.range(0, 10000).parallel().forEach(
+                nbr -> {
+                    try {
+                        Thread thread = new Thread(myRunnable);
+                        thread.start();
+                    } catch (Exception ex) {}
+                    System.out.println(nbr);
+                }
+        );
+
+
 
         return "Generating Object! Use a lot of CPU/Memory";
     }
