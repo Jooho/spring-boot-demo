71a7c50ac71fbe958aebf8a2777a71c51bbf9a5a
Jooho Lee <jlee@redhat.com>
2018-09-22 16:28:49 -0400

    thread print

--- src/main/java/hello/DemoController.java
+++ src/main/java/hello/DemoController.java
@@ -3,6 +3,8 @@ package hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class DemoController {
 
@@ -12,21 +14,26 @@ public class DemoController {
     }
 
     @RequestMapping("/leak")
-    public void populateObjects(){
+    public String populateObjects(){
 
 
         Runnable myRunnable = new Runnable(){
 
             public void run(){
+                int i=0;
                 System.out.println("Creating object");
                 while(true){
+                    ArrayList list = new ArrayList(100);
                     String a = new String();
+                    System.out.println(i++);
                 }
             }
         };
 
         Thread thread = new Thread(myRunnable);
         thread.start();
+
+        return "Generating Object! Use a lot of CPU/Memory";
     }
 
 
