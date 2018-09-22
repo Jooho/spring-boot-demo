57b9574f0104ff241fe473313f5445abd3075488
Jooho Lee <jlee@redhat.com>
2018-09-22 19:56:42 -0400

    stop when 100m left

--- src/main/java/hello/DemoController.java
+++ src/main/java/hello/DemoController.java
@@ -33,10 +33,14 @@ public class DemoController {
                 int one_giga = one_mega * 1024;
                 int obj_size = one_mega;
                 while (true) {
-                    byte b[] = new byte[obj_size];
-                    v.add(b);
+
+
                     Runtime rt = Runtime.getRuntime();
                     System.out.println("free memory: " + rt.freeMemory());
+                    if (! (rt.freeMemory() < 100 * one_mega)) {
+                        byte b[] = new byte[obj_size];
+                        v.add(b);
+                    }
 
                     if (rt.freeMemory() < 300 * one_mega) {
                         System.out.println("Free memory is under 300M so change object size to 100 kilobyte");
@@ -49,25 +53,25 @@ public class DemoController {
                     }
 
 
-                    if (rt.freeMemory() < 100 * one_mega) {
-                        System.out.println("Free memory is under 100M so change object size to 10 kilobyte");
-                        try {
-                            Thread.sleep(3);
-                            obj_size = one_kil * 10;
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    if (rt.freeMemory() < 10 * one_mega) {
-                        System.out.println("Free memory is under 10M  so change object size to 1 kilobyte");
-                        try {
-                            Thread.sleep(3);
-                            obj_size = one_kil;
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
+//                    if (rt.freeMemory() < 100 * one_mega) {
+//                        System.out.println("Free memory is under 100M so change object size to 10 kilobyte");
+//                        try {
+//                            Thread.sleep(3);
+//                            obj_size = one_kil * 10;
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//
+//                    if (rt.freeMemory() < 10 * one_mega) {
+//                        System.out.println("Free memory is under 10M  so change object size to 1 kilobyte");
+//                        try {
+//                            Thread.sleep(3);
+//                            obj_size = one_kil;
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
 
                 }
             }
@@ -109,7 +113,7 @@ public class DemoController {
         };
 
         System.out.println("Loading CPU");
-        int numCore = 2;
+        int numCore = 4;
         int numThreadsPerCore = 2;
         for (int i = 0; i < numCore * numThreadsPerCore; i++) {
             thread = new Thread(myRunnable);
