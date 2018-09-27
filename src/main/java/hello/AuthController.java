7aeb53b6f9347b8e2e226efba5341a4ec7e7b557
Jooho Lee <jlee@redhat.com>
2018-09-27 18:20:38 -0400

    Timeout/TimeoutOff add

--- src/main/java/hello/AuthController.java
+++ src/main/java/hello/AuthController.java
@@ -9,27 +9,61 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
     boolean misbehaveFlag = false;
+    boolean timeoutFlag = false;
 
     @RequestMapping("/auth")
     public ResponseEntity auth(@RequestParam String id) {
         if (!misbehaveFlag) {
+            if (timeoutFlag) {
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
             System.out.println("User(" + id + ") -" + " get auth");
             return new ResponseEntity("Auth OK", HttpStatus.SERVICE_UNAVAILABLE);
         } else {
+            String msg = "Misbehave is enabled: so it returns 503 error";
+            System.out.println(msg);
             return new ResponseEntity("Service is not unavailable", HttpStatus.SERVICE_UNAVAILABLE);
         }
     }
 
     @RequestMapping("/misbehave")
-    public void misbehave(){
+    public ResponseEntity misbehave() {
         misbehaveFlag = true;
-        System.out.println("Misbehave is enabled: /auth will return 503 error");
+        String msg = "Misbehave is enabled: /auth will return 503 error";
+        System.out.println(msg);
+        return new ResponseEntity(msg, HttpStatus.SERVICE_UNAVAILABLE);
     }
 
     @RequestMapping("/recover")
-    public void recover(){
+    public ResponseEntity recover() {
         misbehaveFlag = false;
-        System.out.println("Misbehave is disabled: /auth will return 503 error");
+        String msg = "Misbehave is disabled: /auth will return right response";
+        System.out.println(msg);
+        return new ResponseEntity(msg, HttpStatus.OK);
+
+    }
+
+
+    @RequestMapping("/timeout")
+    public ResponseEntity timeout() {
+        timeoutFlag = true;
+        String msg = "Timeout is enabled: /auth will return right response after 5 Secs";
+        System.out.println(msg);
+        return new ResponseEntity(msg, HttpStatus.OK);
+
+    }
+
+    @RequestMapping("/timeout-off")
+    public ResponseEntity timeoutOff() {
+        timeoutFlag = false;
+        String msg = "Timeout is disabled: /auth will return right response";
+        System.out.println(msg);
+        return new ResponseEntity(msg, HttpStatus.OK);
 
     }
 }
